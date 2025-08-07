1ff74c8ceb577bb564719f1c45cfb31c3e60bfbc
cjaewon <jwn4492@outlook.com>
2025-08-07 23:20:13 +0900

    Create routes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from routes import users, items
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-  return {"message": "Hello World"}
+app.include_router(users.router)
+app.include_router(items.router)
+
